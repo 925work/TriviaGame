d7fe7fbbbc54bd9d8a223fe849dd0b3b14983f06
Alex Pham <925workk@gmail.com>
2019-09-17 22:43:41 -0600

    made start button show questions and answers as well as removing start button. Also created countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,48 @@
 var timeLeft = 80;
-var timeRemaining = 80;
 var correct = 0;
 var wrong = 0;
-var countingDown = false;
+var unanswered = 0;
 var intervalID;
 var questions = [
     {
         question: "Which fictional city is the home of Batman?",
         answers: ["Smallville", "Atlantis", "Metroville", "Gotham"],
-        right: "Gotham"
+        right: 3
     },
     {
         question: "Where does Hank Hill work?",
         answers: ["Propane and Propane Accessories", "Strickland Propane", "Bob's Burgers", "Arlene Propane"],
-        right: "Strickland Propane"
+        right: 1
     },
     {
         question: "What is the powerhouse of the cell?",
         answers: ["Mitochondria", "Chloroplast", "Cell wall", "Nucleus"],
-        right: "Mitochondria"
+        right: 0
     },
     {
         question: "What is the number one cause of death in America?",
         answers: ["Heart disease", "Cancer", "Suicide", "Automotive accidents"],
-        right: "Heart disease"
+        right: 0
     },
     {
         question: "What country first made use of paper money?",
         answers: ["Germany", "United States of America", "China", "Britain"],
-        right: "China"
+        right: 2
     },
     {
         question: "What percent of an egg's weight is its shell?",
         answers: ["50%", "12%", "33%", "20%"],
-        right: "12%"
+        right: 1
     },
     {
         question: "Which of these video games were NOT made by Blizzard?",
         answers: ["World of Warcraft", "Starcraft", "Super Smash Bros Ultimate", "Overwatch"],
-        right: "Super Smash Bros Ultimate"
+        right: 2
     },
     {
         question: "In Tetris, What is the name of the straight line piece?",
         answers: ["Teewee", "Smashboy", "Blue Ricky", "Hero"],
-        right: "Hero"
+        right: 3
     }
 ];
 
@@ -51,22 +50,81 @@ $(".startButton").on("click", start);
 
 function start(){
     clearInterval(intervalID);
-    intervalId = setInterval(decrement, 1000);
+    intervalID = setInterval(decrement, 1000);
     loadQuestions();
+    $(".startButtonContainer").empty();
+    $(".timerContainer").text("Time Left: " + timeLeft);
 }
 
 function decrement(){
     timeLeft--;
     console.log(timeLeft);
+    $(".timerContainer").text("Time Left: " + timeLeft);
+    if(timeLeft===0){
+        stop()
+    }
+}
+
+function stop(){
+    clearInterval(intervalID);
+    showResults();
 }
 
 function loadQuestions(){
-    for( var i =0; i < questions.length; i++){
-        $(".questionsContainer").append("<h4>"+questions[i].question+"<h4>");
-    for( var j =0; j < questions[1].allAnswers.length; j++){
-        $(".questionsContainer").append("<button class=button>"+questions[i].allAnswers[j]+"</button>" )
+    //question 1
+    $(".question0").html("<h1>" + questions[0].question + "</h1>");
+    $(".answers0").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answers[0] + "</label>"
+    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[0].answers[1] + "</label>"
+    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[0].answers[2] + "</label>"
+    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answers[3] + "</label><br><br>");
+    //question 2
+    $(".question1").html("<h1>" + questions[0].question + "</h1>");
+    $(".answers1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[1].answers[0] + "</label>"
+    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[1].answers[1] + "</label>"
+    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[1].answers[2] + "</label>"
+    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[1].answers[3] + "</label><br><br>");
+    //question 3
+    $(".question2").html("<h1>" + questions[2].question + "</h1>");
+    $(".answers2").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[2].answers[0] + "</label>"
+    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[2].answers[1] + "</label>"
+    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[2].answers[2] + "</label>"
+    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[2].answers[3] + "</label><br><br>");
+    //question 4
+    $(".question3").html("<h1>" + questions[3].question + "</h1>");
+    $(".answers3").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[3].answers[0] + "</label>"
+    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[3].answers[1] + "</label>"
+    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[3].answers[2] + "</label>"
+    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[3].answers[3] + "</label><br><br>");
+    //question 5
+    $(".question4").html("<h1>" + questions[4].question + "</h1>");
+    $(".answers4").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[4].answers[0] + "</label>"
+    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[4].answers[1] + "</label>"
+    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[4].answers[2] + "</label>"
+    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[4].answers[3] + "</label><br><br>");
+    //question 6
+    $(".question5").html("<h1>" + questions[5].question + "</h1>");
+    $(".answers5").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[5].answers[0] + "</label>"
+    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[5].answers[1] + "</label>"
+    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[5].answers[2] + "</label>"
+    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[5].answers[3] + "</label><br><br>");
+    //question 7
+    $(".question6").html("<h1>" + questions[6].question + "</h1>");
+    $(".answers6").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[6].answers[0] + "</label>"
+    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[6].answers[1] + "</label>"
+    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[6].answers[2] + "</label>"
+    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[6].answers[3] + "</label><br><br>");
+    //question 8
+    $(".question7").html("<h1>" + questions[7].question + "</h1>");
+    $(".answers7").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[7].answers[0] + "</label>"
+    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[7].answers[1] + "</label>"
+    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[7].answers[2] + "</label>"
+    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[7].answers[3] + "</label><br><br>");
 }
 
 function tryAgain() {
     location.reload();
+}
+
+function showResults (){
+
 }
