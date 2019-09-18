83431a6b70d6b01c9471a8cf0ebbf2046ebf2c58
Alex Pham <925workk@gmail.com>
2019-09-18 11:05:27 -0600

    fixed submit button not working as well as radio answers not being unique to questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -49,6 +49,7 @@ var questions = [
 $(".startButton").on("click", start);
 $(".tryAgain").on("click", tryAgain);
 
+
 function start(){
     clearInterval(intervalID);
     intervalID = setInterval(decrement, 1000);
@@ -62,7 +63,8 @@ function decrement(){
     console.log(timeLeft);
     $(".timerContainer").text("Time Left: " + timeLeft);
     if(timeLeft===0){
-        stop()
+        stop();
+        scoring();
     }
 }
 
@@ -74,10 +76,10 @@ function stop(){
 function loadQuestions(){
     //question 1
     $(".question0").html("<h1>" + questions[0].question + "</h1>");
-    $(".answers0").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answers[0] + "</label>"
-    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[0].answers[1] + "</label>"
-    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[0].answers[2] + "</label>"
-    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answers[3] + "</label><br><br>");
+    $(".answers0").html("<input type='radio' name='answer0' value='0'>" + "<label>" + questions[0].answers[0] + "</label>"
+    + "<input type='radio' name='answer0' value='1'>" + "<label>" + questions[0].answers[1] + "</label>"
+    + "<input type='radio' name='answer0' value='2'>" + "<label>" + questions[0].answers[2] + "</label>"
+    + "<input type='radio' name='answer0' value='3'>" + "<label>" + questions[0].answers[3] + "</label><br><br>");
     //question 2
     $(".question1").html("<h1>" + questions[0].question + "</h1>");
     $(".answers1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[1].answers[0] + "</label>"
@@ -86,41 +88,52 @@ function loadQuestions(){
     + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[1].answers[3] + "</label><br><br>");
     //question 3
     $(".question2").html("<h1>" + questions[2].question + "</h1>");
-    $(".answers2").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[2].answers[0] + "</label>"
-    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[2].answers[1] + "</label>"
-    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[2].answers[2] + "</label>"
-    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[2].answers[3] + "</label><br><br>");
+    $(".answers2").html("<input type='radio' name='answer2' value='0'>" + "<label>" + questions[2].answers[0] + "</label>"
+    + "<input type='radio' name='answer2' value='1'>" + "<label>" + questions[2].answers[1] + "</label>"
+    + "<input type='radio' name='answer2' value='2'>" + "<label>" + questions[2].answers[2] + "</label>"
+    + "<input type='radio' name='answer2' value='3'>" + "<label>" + questions[2].answers[3] + "</label><br><br>");
     //question 4
     $(".question3").html("<h1>" + questions[3].question + "</h1>");
-    $(".answers3").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[3].answers[0] + "</label>"
-    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[3].answers[1] + "</label>"
-    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[3].answers[2] + "</label>"
-    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[3].answers[3] + "</label><br><br>");
+    $(".answers3").html("<input type='radio' name='answer3' value='0'>" + "<label>" + questions[3].answers[0] + "</label>"
+    + "<input type='radio' name='answer3' value='1'>" + "<label>" + questions[3].answers[1] + "</label>"
+    + "<input type='radio' name='answer3' value='2'>" + "<label>" + questions[3].answers[2] + "</label>"
+    + "<input type='radio' name='answer3' value='3'>" + "<label>" + questions[3].answers[3] + "</label><br><br>");
     //question 5
     $(".question4").html("<h1>" + questions[4].question + "</h1>");
-    $(".answers4").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[4].answers[0] + "</label>"
-    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[4].answers[1] + "</label>"
-    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[4].answers[2] + "</label>"
-    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[4].answers[3] + "</label><br><br>");
+    $(".answers4").html("<input type='radio' name='answer4' value='0'>" + "<label>" + questions[4].answers[0] + "</label>"
+    + "<input type='radio' name='answer4' value='1'>" + "<label>" + questions[4].answers[1] + "</label>"
+    + "<input type='radio' name='answer4' value='2'>" + "<label>" + questions[4].answers[2] + "</label>"
+    + "<input type='radio' name='answer4' value='3'>" + "<label>" + questions[4].answers[3] + "</label><br><br>");
     //question 6
     $(".question5").html("<h1>" + questions[5].question + "</h1>");
-    $(".answers5").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[5].answers[0] + "</label>"
-    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[5].answers[1] + "</label>"
-    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[5].answers[2] + "</label>"
-    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[5].answers[3] + "</label><br><br>");
+    $(".answers5").html("<input type='radio' name='answer5' value='0'>" + "<label>" + questions[5].answers[0] + "</label>"
+    + "<input type='radio' name='answer5' value='1'>" + "<label>" + questions[5].answers[1] + "</label>"
+    + "<input type='radio' name='answer5' value='2'>" + "<label>" + questions[5].answers[2] + "</label>"
+    + "<input type='radio' name='answer5' value='3'>" + "<label>" + questions[5].answers[3] + "</label><br><br>");
     //question 7
     $(".question6").html("<h1>" + questions[6].question + "</h1>");
-    $(".answers6").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[6].answers[0] + "</label>"
-    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[6].answers[1] + "</label>"
-    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[6].answers[2] + "</label>"
-    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[6].answers[3] + "</label><br><br>");
+    $(".answers6").html("<input type='radio' name='answer6' value='0'>" + "<label>" + questions[6].answers[0] + "</label>"
+    + "<input type='radio' name='answer6' value='1'>" + "<label>" + questions[6].answers[1] + "</label>"
+    + "<input type='radio' name='answer6' value='2'>" + "<label>" + questions[6].answers[2] + "</label>"
+    + "<input type='radio' name='answer6' value='3'>" + "<label>" + questions[6].answers[3] + "</label><br><br>");
     //question 8
     $(".question7").html("<h1>" + questions[7].question + "</h1>");
-    $(".answers7").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[7].answers[0] + "</label>"
-    + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[7].answers[1] + "</label>"
-    + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[7].answers[2] + "</label>"
-    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[7].answers[3] + "</label><br><br>");
+    $(".answers7").html("<input type='radio' name='answer7' value='0'>" + "<label>" + questions[7].answers[0] + "</label>"
+    + "<input type='radio' name='answer7' value='1'>" + "<label>" + questions[7].answers[1] + "</label>"
+    + "<input type='radio' name='answer7' value='2'>" + "<label>" + questions[7].answers[2] + "</label>"
+    + "<input type='radio' name='answer7' value='3'>" + "<label>" + questions[7].answers[3] + "</label><br><br>");
     $(".questionsContainer").append("<button class='submit'>" + "SUBMIT" +"</button>")
+    $(".submit").on("click", function(){
+        scoring();
+        showResults();
+    });
+}
+
+function scoring(){
+    var userAnswer0 = $("input[name='answer0']:checked").val();
+    if(userAnswer0 === undefined){
+        unanswered++;
+    } 
 }
 
 function tryAgain() {
